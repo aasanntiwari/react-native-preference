347dcbdb202f478a799d9a67ce1ef73ce4da4781
mac mi <aasann.tiwari@spraxa.com>
2023-01-11 21:05:28 +0530

    bug fixed

--- Example/ios/Example/AppDelegate.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
-@property (copy, nonatomic) NSDictionary *launchOptions;
-@property (strong, nonatomic) UIWindow *window;
-@end
